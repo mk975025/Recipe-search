4c7621d94c60e108590c261791be02f321aeb1a1
Matthew Kim <matthew.kim.733@my.csun.edu>
2021-07-15 21:04:50 -0700

    nutritional API

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Recipe from "./components/Recipe";
+import Nutrition from "./components/Nutrition";
 
 function App() {
+  const [nutritionSearch, setNutritionSearch] = useState("milk");
+  const [nutrition, setNutrition] = useState([]);
+
   const [search, setSearch] = useState("cookies");
   const [loading, setLoading] = useState(false);
   const [recipes, setRecipes] = useState([]);
@@ -10,9 +14,13 @@ function App() {
   const API_KEY = "0ea4476c15af3a504a8878f0b3cbbbe6";
   const API_ID = "3d45a097";
 
+  const NUTRITION_API_KEY = "b341aafccfc844c75dafbec9590327c8";
+  const NUTRITION_API_ID = "4656ed26";
+
   useEffect(() => {
     setLoading(true);
     fetchRecipes();
+    fetchNutrition();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const fetchRecipes = async () => {
@@ -24,6 +32,15 @@ function App() {
     // console.log(data.hits);
     setLoading(false);
   };
+  const fetchNutrition = async() => {
+    const response = await fetch(
+        `https://api.edamam.com/api/nutrition-data?app_id=${NUTRITION_API_ID}&app_key=${NUTRITION_API_KEY}&nutrition-type=cooking&ingr=${nutritionSearch}`
+    ); const data = await response.json();
+    setNutrition(data.healthLabels);
+    console.log(data.healthLabels);
+    setLoading(false);
+  }
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -32,7 +49,7 @@ function App() {
       <h1 className="title">Recipe App</h1>
       {typeof recipes[0] !== "undefined" && (
         <div className="recipe">
-          {recipes.map((recipe) => (
+          {recipes.map((recipe, index) => (
             <Recipe
               key={recipe.recipe.label}
               title={recipe.recipe.label}
@@ -41,10 +58,14 @@ function App() {
               ingredients={recipe.recipe.ingredients}
             />
           ))}
+          {nutrition.map((label, index) => (<Nutrition
+            key={index}
+            healthLabel={label}
+          />))}
         </div>
       )}
     </div>
   );
-}
+          }
 
 export default App;
---
+++ recipes/src/components/Nutrition.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nutrition({healthLabel}) {
+  return (
+    <div>
+        {healthLabel}
+    </div>
+  );
+}
